21ec3b285fbff0a4073e8f57e8e18c94d7b95266
yutorikohei <yutoriseikatu49@gmail.com>
2021-07-18 09:52:41 +0900

    hello

--- app/.history/src/test/java/sequality/AppTest_20210712093118.java
+++ app/.history/src/test/java/sequality/AppTest_20210712093118.java
@@ -7,9 +7,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test 
-    public void testAppReternHelloWorld() {
+    @Test public void testAppHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("Hello World", classUnderTest.getGreeting());
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
